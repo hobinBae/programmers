62db2f63d4460fd230d8746f8fa432decc697105
hobinBae <123109572+hobinBae@users.noreply.github.com>
2024-09-23 17:52:17 +0900

    [level 1] Title: 바탕화면 정리, Time: 3.91 ms, Memory: 64.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] solution(String[] wallpaper) {
+        int lux = Integer.MAX_VALUE;
+        int luy = Integer.MAX_VALUE;
+
+        int rdx = Integer.MIN_VALUE;
+        int rdy = Integer.MIN_VALUE;
+        for(int i = 0; i < wallpaper.length; i++) {
+            String[] str = wallpaper[i].split("");
+            for (int j = 0; j < str.length; j++) {
+                if (str[j].equals("#")) {
+                    lux = Math.min(lux, i);
+                    luy = Math.min(luy, j);
+                    rdx = Math.max(rdx, i);
+                    rdy = Math.max(rdy, j);
+                }
+            }
+        }
+
+        int[] answer = {lux, luy, rdx + 1, rdy + 1};
+        return answer;
+
+    }
+}
