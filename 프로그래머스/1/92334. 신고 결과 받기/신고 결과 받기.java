9d93d484d906b3619540c94dcb4a2213820e098e
hobinBae <123109572+hobinBae@users.noreply.github.com>
2024-09-26 15:18:46 +0900

    [level 1] Title: 신고 결과 받기, Time: 237.91 ms, Memory: 175 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,6 +1,43 @@
+import java.util.*;
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        int[] answer = {};
+        HashMap<String, HashSet<String>> map = new HashMap<>();
+        HashMap<String, Integer> count = new HashMap<>();
+        List<String> list = new ArrayList<>();
+        int[] answer =  new int[id_list.length];
+        for (String s : id_list) {
+            count.put(s, k);
+            map.put(s, new HashSet<>());
+        }
+
+        for (String s : report) {
+            String[] str = s.split(" ");
+            map.get(str[0]).add(str[1]);
+        }
+
+        for (String s : map.keySet()) {
+            for (String string : map.get(s)) {
+                if (count.containsKey(string)) {
+                    count.put(string, count.get(string) - 1);
+                }
+            }
+        }
+        for (String s : count.keySet()) {
+          if(count.get(s) <= 0){
+              list.add(s);
+          }
+        }
+
+        for(int i = 0 ; i < id_list.length; i++){
+            int sum = 0;
+            for (String s : list) {
+                if(map.get(id_list[i]).contains(s)){
+                    sum++;
+                }
+            }
+            answer[i] = sum;
+        }
         return answer;
     }
 }
+    
