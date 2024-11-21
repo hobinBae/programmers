f7b4ec80a2271cca9a608b4e64cb43ffbaf0eb96
hobinBae <123109572+hobinBae@users.noreply.github.com>
2024-11-21 21:22:45 +0900

    [Bronze I] Title: 평균, Time: 116 ms, Memory: 14652 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int max = Integer.MIN_VALUE;
+        double[] arr = new double[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(max, (int)arr[i]);
+        }
+        for (int i = 0; i < N; i++) {
+            arr[i] = arr[i] / max * 100;
+        }
+         double sum = 0;
+        for (double v : arr) {
+            sum += v;
+        }
+        System.out.printf("%.6f", sum / N);
+    }
+}
