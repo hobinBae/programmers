edd0a2b66089b6ff77568421669302e60bfc410b
hobinBae <123109572+hobinBae@users.noreply.github.com>
2024-11-22 01:45:47 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 192 ms, Memory: 17944 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception{
+        Scanner scan = new Scanner(System.in);
+        while(scan.hasNext()){
+            String line = scan.nextLine();
+            System.out.println(line);
+        }
+    }
+}
