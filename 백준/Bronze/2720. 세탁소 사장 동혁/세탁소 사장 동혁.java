9879d849f4276a9e07b1051964de22b60df45086
hobinBae <123109572+hobinBae@users.noreply.github.com>
2024-11-26 21:22:34 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 168 ms, Memory: 15404 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -6,26 +6,24 @@ public class Main {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-        double quarter = 0.25 * 100;
-        double dime = 0.10 * 100;
-        double nickel = 0.05 * 100;
-        double penny = 0.01 * 100;
+        int quarter = 25;
+        int dime = 10;
+        int nickel = 5;
+        int penny = 1;
         int[] arr = new int[4];
         for (int test_case = 1; test_case <= T; test_case++) {
             int index = 0;
             int money = Integer.parseInt(br.readLine());
-           
-
-            arr[index++] =(int)(money / quarter);
+            arr[index++] = money / quarter;
             money %= quarter;
 
-            arr[index++] = (int) (money / dime);
+            arr[index++] = money / dime;
             money %= dime;
 
-            arr[index++] = (int) (money / nickel);
+            arr[index++] = money / nickel;
             money %= nickel;
             
-            arr[index++] = (int) (money / penny);
+            arr[index++] = money / penny;
             money %= penny;
 
             for (int i : arr) {
