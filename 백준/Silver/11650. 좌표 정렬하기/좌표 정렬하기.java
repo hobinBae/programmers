c87089eaf892c259561860b9659ff158fd836d6d
hobinBae <123109572+hobinBae@users.noreply.github.com>
2024-12-09 23:06:29 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1172 ms, Memory: 57256 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static class Node {
+        private int x;
+        private int y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+        public int getX(){
+            return this.x;
+        }
+        public int getY(){
+            return this.y;
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        ArrayList<Node> list = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            list.add(new Node(x, y));
+        }
+        Collections.sort(list, (o1, o2) -> {
+            if (o1.getX() == o2.getX()) {
+                return Integer.compare(o1.getY(), o2.getY());
+            }else{
+                return Integer.compare(o1.getX(), o2.getX());
+            }
+        });
+        for (Node node : list) {
+            System.out.println(node.getX() + " " + node.getY());
+        }
+
+    }
+
+}
