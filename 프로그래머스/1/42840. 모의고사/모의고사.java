1bd2a8f5075599258d0dd50484b1fe7ec95d3f42
hobinBae <123109572+hobinBae@users.noreply.github.com>
2024-12-18 16:31:31 +0900

    [level 1] Title: 모의고사, Time: 4.30 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,33 +1,34 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] answers) {
-      int[] answer1 = {1, 2, 3, 4, 5};
-        int[] good = new int[4];
-        int[] answer2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] answer3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        List<Integer> list = new ArrayList<>();
-        for(int i = 0; i < answers.length; i++){
-            if(answers[i] == answer1[i % answer1.length]){
-                good[1]++;
-            }
-            if(answers[i] == answer2[i % answer2.length]){
-                good[2]++;
-            }
-            if(answers[i] == answer3[i % answer3.length]){
-                good[3]++;
-            }
-        }
+            int[][] current = {
+                {1, 2, 3, 4, 5},
+                {2, 1, 2, 3, 2, 4, 2, 5},
+                {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+        };
+        int[] answer = new int[3];
+        ArrayList<Integer>list = new ArrayList<>();
         int max = Integer.MIN_VALUE;
-        for(int i = 1; i < good.length; i++){
-            max = Math.max(max, good[i]);
+        for (int i = 0; i < current.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < answers.length; j++) {
+                if(answers[j] == current[i][j % current[i].length]){
+                    sum++;
+                }
+            }
+            answer[i] = sum;
+     
+            max = Math.max(max, sum);
         }
-
-        for(int i = 0; i < good.length; i++){
-            if(max == good[i]){
-                list.add(i);
+        for (int i = 0; i < answer.length; i++) {
+            if(answer[i] == max){
+                list.add(i + 1);
             }
         }
-
         return list.stream().mapToInt(Integer::intValue).toArray();
+
+
+    
     }
+
 }
