4cf197226aa230c604c148ab7df89182dbf5e99a
hobinBae <123109572+hobinBae@users.noreply.github.com>
2025-02-10 11:22:16 +0900

    [Bronze II] Title: 방 배정, Time: 100 ms, Memory: 14004 KB, Score: 2 point -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		HashMap<Integer, Integer> map0 = new HashMap<>();
+		HashMap<Integer, Integer> map1 = new HashMap<>();
+		
+		for(int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			if(a == 0) {
+				map0.put(b, map0.getOrDefault(b, 0) + 1);
+			}else {
+				map1.put(b, map1.getOrDefault(b, 0) + 1);
+			}
+		}
+		int sum = 0;
+		for(int i : map0.keySet()) {
+			int value = map0.get(i);
+			if(value <= K) {
+				sum += 1;
+			}else if(value / K % K == 0 ) {
+				sum += value / K;
+			}else {
+				sum += value / K + 1;
+			}
+			
+		}
+		for(int i : map1.keySet()) {
+			int value = map1.get(i);
+			if(value <= K) {
+				sum += 1;
+			}else if(value / K % K == 0) {
+				
+			}else {
+				sum += value / K + 1;
+			}
+		
+			
+		}
+
+		System.out.println(sum);
+	}
+	
+}
