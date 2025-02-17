68756ec7cc40330c14877a65b4719e0791494b18
hobinBae <123109572+hobinBae@users.noreply.github.com>
2025-02-17 22:14:02 +0900

    [Silver III] Title: N과 M (2), Time: 104 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    static int N;
+    static int M;
+    static boolean[] used;
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st =new StringTokenizer(br.readLine());
+         N = Integer.parseInt(st.nextToken());
+         M = Integer.parseInt(st.nextToken());
+        used = new boolean[N + 1];
+        sb = new StringBuilder();
+        backtrack(0, 0, "");
+        System.out.println(sb);
+    }
+
+    private static void backtrack( int num , int cnt, String s){
+        if (cnt == M) {
+            sb.append(s.trim() + "\n");
+        }
+        for(int i = num + 1; i <= N; i++){
+            if(!used[i]){
+                used[i] = true;
+                backtrack(i, cnt + 1,  s + " " + i );
+                used[i] = false;
+            }
+        }
+    }
+}
