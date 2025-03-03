f41b222acb6d3f117bc8a70186e2ff1713439d29
hobinBae <123109572+hobinBae@users.noreply.github.com>
2025-03-03 17:11:26 +0900

    [Gold IV] Title: N-Queen, Time: 2624 ms, Memory: 14672 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -2,33 +2,31 @@ import java.util.*;
 import java.io.*;
 public class Main {
     static int N;
-    static int answer;
     static boolean[] col, slash, bslash;
-
+    static int answer;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         col = new boolean[N + 1];
         slash = new boolean[2 * N + 1];
         bslash = new boolean[2 * N + 1];
+        answer = 0;
         backtrack(1);
         System.out.println(answer);
-
     }
     static void backtrack(int row){
-    if(row > N){
-        answer++;
-        return;
-    }
-        for(int c = 1; c <= N; c++){
-
-            if(!check(row, c)) continue;
-            col[c] = slash[row + c] = bslash[(row - c) + N] = true;
+        if(row > N){
+            answer++;
+            return;
+        }
+        for(int i = 1; i <= N; i++){
+            if(!check(row, i))continue;
+            col[i] = slash[row + i] = bslash[(row - i) + N] = true;
             backtrack(row + 1);
-            col[c] = slash[row + c] = bslash[(row - c) + N] = false;
+            col[i] = slash[row + i] = bslash[(row - i) + N] = false;
         }
     }
-    static boolean check(int row, int c){
+    static boolean check(int row,int c){
         return !col[c] && !slash[row + c] && !bslash[(row - c) + N];
     }
 }
