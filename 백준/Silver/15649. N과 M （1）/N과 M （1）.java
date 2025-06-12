f2f12e6f4fcad53671d5a77a11063eab59acb60b
hobinBae <123109572+hobinBae@users.noreply.github.com>
2025-06-12 15:55:49 +0900

    [Silver III] Title: N과 M (1), Time: 268 ms, Memory: 42812 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -4,28 +4,34 @@ public class Main {
     static int N;
     static int M;
     static boolean[] used;
-    static StringBuilder sb;
-    public static void main(String[] args) throws IOException{
+    static ArrayList<Integer> list;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st =new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
          N = Integer.parseInt(st.nextToken());
          M = Integer.parseInt(st.nextToken());
-        used = new boolean[N + 1];
-        sb = new StringBuilder();
-        backtrack(0, "");
+         used = new boolean[N + 1];
+         list = new ArrayList<>();
+         back(0);
         System.out.println(sb);
     }
 
-    private static void backtrack( int cnt, String s){
-        if (cnt == M) {
-            sb.append(s.trim() + "\n");
+    static void back(int cnt){
+        if(cnt == M){
+            for (Integer i : list) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
         }
         for(int i = 1; i <= N; i++){
             if(!used[i]){
                 used[i] = true;
-                backtrack(cnt + 1,  s + " " + i );
+                list.add(i);
+                back(cnt + 1);
                 used[i] = false;
+                list.remove(list.size() - 1);
             }
         }
     }
