1f1dc791bc5b3ad8fe5b973297de17a2e9b6c4cb
hobinBae <123109572+hobinBae@users.noreply.github.com>
2025-12-01 22:13:30 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,10 +1,10 @@
 class Solution {
     public int solution(int[] number) {
-             int N = number.length;
+        // 3명의 정수 번호 합이 0이 되는 개수
         int answer = 0;
-        for(int i = 0; i < N; i++){
-            for(int j = i + 1; j < N; j++){
-                for(int k = j + 1; k < N; k++){
+        for(int i = 0 ; i < number.length; i++){
+            for(int j = i + 1; j < number.length; j++){
+                for(int k = j + 1; k < number.length; k++){
                     if(number[i] + number[j] + number[k] == 0){
                         answer++;
                     }
