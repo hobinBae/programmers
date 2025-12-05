4b5960a13f5123379208f7519daa24cae3a6dcb1
hobinBae <123109572+hobinBae@users.noreply.github.com>
2025-12-05 18:57:34 +0900

    [level 1] Title: 예산, Time: 0.51 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -1,21 +1,6 @@
-import java.util.*;
 class Solution {
-    
     public int solution(int[] d, int budget) {
-        Arrays.sort(d);
-        int count = 0;
-        
-        for(int i : d){
-            if(budget - i >= 0){
-                budget -= i;
-                count++;
-            }else{
-                break;
-            }
-                
-        }
-        
-        return count;
+        int answer = 0;
+        return answer;
     }
-
 }
