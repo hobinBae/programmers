2a2c0294da239aee33d862775086a84fb198dbe7
hobinBae <123109572+hobinBae@users.noreply.github.com>
2025-12-15 15:32:28 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 8.92 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,12 +1,33 @@
 class Solution {
+    
+     static StringBuilder sb = new StringBuilder();
+    
     public String solution(int[] food) {
-          StringBuilder sb = new StringBuilder();
+        
         for(int i = 1; i < food.length; i++){
-            int length = food[i] / 2;
-            for(int j = 0; j < length; j++){
-                sb.append(i);
+            
+            if(food[i] == 1){
+                continue;
+            }
+            
+            if(food[i] % 2 == 0){
+                addFood(food[i] / 2, i + "");
+            }
+            else{
+                addFood((food[i] - 1) / 2, i + "");
             }
         }
-        return sb.toString() + "0" + sb.reverse();
+        String answer = sb.toString();
+        answer += "0";
+        answer += sb.reverse();
+        
+        return answer;
     }
+    
+    static void addFood(int n, String k){
+        for(int i = 0; i < n ; i++){
+            sb.append(k);
+        }
+    }
+    
 }
