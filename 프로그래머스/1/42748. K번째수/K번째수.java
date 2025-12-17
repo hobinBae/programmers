964cb50374b8f4536fd4159c976625cc50e580c1
hobinBae <123109572+hobinBae@users.noreply.github.com>
2025-12-17 23:37:03 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,14 +1,18 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-       int[] answer = new int[commands.length];
-        int index = 0;
-        for (int[] arr : commands) {
-            int[] copyArray = Arrays.copyOfRange(array, arr[0] -1, arr[1]);
-            Arrays.sort(copyArray);
-            answer[index++] = copyArray[arr[2] -1];
+        
+        int[] answer = new int[commands.length];
+        
+        for(int i = 0 ; i < commands.length; i++){
+            ArrayList<Integer> list = new ArrayList<>();
+            
+            for(int j = commands[i][0] - 1; j <= commands[i][1] - 1; j++){
+                list.add(array[j]);
+            }
+            Collections.sort(list);
+            answer[i] = list.get(commands[i][2] - 1);
         }
-
         return answer;
     }
 }
