d726473396b0e9803066efad662cc491743dab03
hobinBae <123109572+hobinBae@users.noreply.github.com>
2025-12-21 16:42:03 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.05 ms, Memory: 66.9 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -2,15 +2,14 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
+        String bit = Integer.toString(n, 2);
         int answer = 0;
-        while(n > 0){
-            if(n % 2 == 0){
-                n /= 2;
-            }else if(n % 2 == 1){
-                n -= 1;
+        for(int i = 0; i < bit.length(); i++){
+            if(bit.charAt(i) == '1'){
                 answer++;
             }
         }
+
         return answer;
     }
 }
