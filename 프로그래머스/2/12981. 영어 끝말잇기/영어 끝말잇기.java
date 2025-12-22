dccff388bbb7b98d3b6d19cd8958702e576c9c77
hobinBae <123109572+hobinBae@users.noreply.github.com>
2025-12-22 17:15:58 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,24 +1,41 @@
 import java.util.*;
 class Solution {
     public int[] solution(int n, String[] words) {
-   HashSet<String>usedWords = new HashSet<>();
-        char end = ' ';
-        for(int i = 0; i < words.length; i++){
-            if(usedWords.isEmpty()){
-                usedWords.add(words[i]);
-                end = words[i].charAt(words[i].length()-1);
-                continue;
+        int[] answer = {0, 0};
+        int idx = 0;
+        int turn = 0;
+        int caught = 0;
+        HashSet<String> set = new HashSet<>();
+        char lastWord = ' ';
+        while(true){
+            if(idx == words.length){
+                break;
             }
-
-            if(usedWords.contains(words[i]) || end != words[i].charAt(0)){
-                return new int[]{(i % n) + 1, i / n + 1};
+            char firstWord = words[idx].charAt(0);
+            turn = idx % n;
+            if(idx == 0){
+                lastWord = words[idx].charAt(words[idx].length() - 1);
             }else{
-                usedWords.add(words[i]);
-                end = words[i].charAt(words[i].length() - 1);
-            }
+                if(firstWord != lastWord){
+                    if(idx == 0){
+                        idx++;
+                    }
 
+                    return new int[] {turn + 1, (idx / n) + 1};
+                }else{
+                    lastWord = words[idx].charAt(words[idx].length() - 1);
+                }
+            }
+            if(!set.add(words[idx])){
+                if(idx == 0){
+                        idx++;
+                    }
+                return new int[] {turn + 1, (idx / n) + 1};
+            }
+            idx++;
+            
         }
+
         return new int[]{0, 0};
     }
-    
 }
