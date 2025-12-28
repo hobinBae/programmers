d5b8d726fe74f676c94b7d4925bacf7d7f08ea5a
hobinBae <123109572+hobinBae@users.noreply.github.com>
2025-12-28 15:08:08 +0900

    [level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/README.md
+++ 프로그래머스/2/12985. 예상 대진표/README.md
@@ -1,10 +1,10 @@
 # [level 2] 예상 대진표 - 12985 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12985) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12985#) 
 
 ### 성능 요약
 
-메모리: 79.2 MB, 시간: 0.02 ms
+메모리: 87.9 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 02일 17:52:21
+2025년 12월 28일 15:08:05
 
 ### 문제 설명
 
--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -2,13 +2,32 @@ class Solution
 {
     public int solution(int n, int a, int b)
     {
-        int answer = 0;
-        for(answer = 0; a != b; answer++){
-            a = (a + 1) / 2;
-            b = (b + 1) / 2;
+        int answer = 1;
+        while(a > 0){
+            //a가 짝수인지 확인
+            if(a % 2 == 0){
+                if(b == a - 1){
+                    return answer;
+                }
+                else{
+                    a /= 2;
+                    b = b % 2 == 0 ? b / 2 : b / 2 + 1;
+                    // System.out.println("a가 짝수일 때" + " " + a + " " + b);
+                    answer++;
+                }
+            }
+            else if(a % 2 == 1){
+                if(b == a + 1){
+                    return answer;
+                }
+                else{
+                    a = a / 2 + 1;
+                    b = b % 2 == 0 ? b / 2 : b / 2 + 1;
+                    answer++;
+                    // System.out.println("a가 홀수 일 때" + " " + a + " " + b);
+                }
+            }
         }
-        
-
 
         return answer;
     }
