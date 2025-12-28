0bb8cd525ab8452a22726b5b35ff1293b112ae7b
hobinBae <123109572+hobinBae@users.noreply.github.com>
2025-12-28 16:46:52 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 560 ms, Memory: 142184 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+        int answer = 0;
+        ArrayList<Integer>[] graph = new ArrayList[V + 1];
+        boolean[] visited = new boolean[V + 1];
+        for(int i = 1; i <= V; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        for(int i = 1; i <= E; i++){
+            st = new StringTokenizer(br.readLine());
+            int v = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+
+            graph[v].add(e);
+            graph[e].add(v);
+        }
+
+        for(int i = 1; i <= V; i++){
+            if(visited[i]){
+                continue;
+            }
+            answer++;
+            ArrayDeque<Integer> dq = new ArrayDeque<>();
+
+            dq.add(i);
+            visited[i] = true;
+
+            while (!dq.isEmpty()) {
+                int now = dq.poll();
+                for (int j : graph[now]) {
+                    if(visited[j]){
+                        continue;
+                    }
+                    dq.add(j);
+                    visited[j] = true;
+                }
+            }
+
+
+        }
+        System.out.println(answer);
+
+    }
+}
