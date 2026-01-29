841770ac0a1b49c33a546ad3255d90d4b91684d6
hobinBae <123109572+hobinBae@users.noreply.github.com>
2026-01-29 16:11:23 +0900

    [level 2] Title: 할인 행사, Time: 62.90 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/README.md
+++ 프로그래머스/2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127?language=java) 
 
 ### 성능 요약
 
-메모리: 105 MB, 시간: 34.50 ms
+메모리: 109 MB, 시간: 62.90 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 08일 13:31:00
+2026년 01월 29일 16:11:19
 
 ### 문제 설명
 
--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,34 +1,57 @@
 import java.util.*;
 class Solution {
+    /*
+    discount의 최대 크기는 100,000
+    want, number의 길이는 최대 10
+    그래서 10,000,000 이라 시간은 터지지 않을듯
+    
+    해시맵을 이용해서 인덱스를 저장
+    반복문을 돌면서 해당 인덱스에 ++
+    반복문을 다 돌면 해당 인덱스의 크기를 비교
+    같거나 크다면 다음으로 
+    작으면 break
+    
+    성공 여부에 따른 boolean을 만들고 성공하면 인덱스 저장
+    실패하면 false
+
+    인덱스 크기만큼
+    */
     public int solution(String[] want, int[] number, String[] discount) {
-        HashMap<String, Integer> w = new HashMap<>();
-        HashMap<String, Integer> d = new HashMap<>();
-        
         int answer = 0;
+        HashMap<String, Integer> map = new HashMap<>();
+        
         for(int i = 0; i < want.length; i++){
-            w.put(want[i], number[i]);
+            map.put(want[i], i);
+        }
+        int length = 0;
+        for(int i = 0; i < number.length; i++){
+            length += number[i];
+        }
+        for(int i = 0; i <= discount.length - length; i++){
+            boolean flag = true;
+            int[] arr = new int[want.length];
+            
+            for(int j = i; j < i + length; j++){
+                if(map.containsKey(discount[j])){
+                    arr[map.get(discount[j])]++;
+                }
+            }
+
+            for(int j = 0; j < arr.length; j++){
+                if(arr[j] < number[j]){
+                    flag = false;
+                    break;
+                }
+            }
+            
+            if(flag){
+              System.out.print(i);
+              answer++;
+           }
         }
         
-        for(int i = 0; i < 10; i++){
-            d.put(discount[i], d.getOrDefault(discount[i], 0) + 1);
-}
-        if(valid(w, d, want)){
-            answer++;
-        }
-        for(int i = 10; i < discount.length; i++){
-            d.put(discount[i - 10], d.get(discount[i - 10]) - 1); 
-            d.put(discount[i], d.getOrDefault(discount[i], 0) + 1);
-            if(valid(w, d, want)){
-                answer++;
-            }
-        }
+        
         return answer;
     }
-    private static boolean valid(HashMap<String, Integer> w, HashMap<String, Integer> d, String[] want){
-        for(String s : want){
-            if(d.getOrDefault(s, 0) < w.get(s))
-                return false;
-        }
-        return true;
-    }
+    
 }
